3e9f05bb1517e4a9a63ef93007176abc4335cabc
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2022-11-12 09:31:59 +0700

    create new migration

---
+++ app/Database/Migrations/2022-11-11-082336_CreateNotaTable.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateNotaTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_pengeluaran' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'tanggal_pengeluaran' => [
+                'type'       => 'DATE',
+            ],
+            'jumlah_pengeluaran' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at' => [
+                'type'       => 'DATETIME',
+                'null'       => 'true',
+            ],
+            'updated_at' => [
+                'type'       => 'DATETIME',
+                'null'       => 'true',
+            ],
+            'employee_id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+            ],
+        ]);
+        $this->forge->addKey('id_pengeluaran', true);
+        $this->forge->createTable('pengeluaran');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('nota');
+    }
+}
