b35600f23519a1cdf9031a50219c7a04790be1c5
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2022-11-12 09:31:47 +0700

    Create NotaController.php

---
+++ app/Controllers/NotaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class NotaController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
